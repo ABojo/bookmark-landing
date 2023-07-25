716b342a5f691d05d920034068642b0e371dd280
ABojo <Alex.bojorquez2598@gmail.com>
2023-07-24 22:17:07 -0400

    added javascript for handling the validation and submission of the contact form

---
+++ dist/js/index.js
@@ -0,0 +1,53 @@
+const classes = {
+  form: ".contact__form",
+  formSubmitted: "contact__form--submitted",
+  input: ".contact__input",
+  inputError: "contact__input--error",
+};
+
+//elements
+const formElement = document.querySelector(classes.form);
+const formInputElement = document.querySelector(classes.input);
+
+function isValidEmail(email) {
+  const emailRegex =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  return email.match(emailRegex);
+}
+
+//display controller
+const display = (function () {
+  function showInputError() {
+    formInputElement.classList.add(classes.inputError);
+  }
+
+  function removeInputError() {
+    formInputElement.classList.remove(classes.inputError);
+  }
+
+  function showFormSuccess() {
+    formElement.classList.add(classes.formSubmitted);
+  }
+
+  return { showInputError, removeInputError, showFormSuccess };
+})();
+
+//listener to display errors or success state
+formElement.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const formData = new FormData(formElement);
+  const email = formData.get("email");
+
+  if (isValidEmail(email)) {
+    display.showFormSuccess();
+  } else {
+    display.showInputError();
+  }
+});
+
+//clear errors after new input
+formInputElement.addEventListener("input", () => {
+  display.removeInputError();
+});
